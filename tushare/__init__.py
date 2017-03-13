5369790c6f7e82311c44c4f60cc08f39fd65a273
可达书院 <alancheng618@hotmail.com>
2017-03-14 00:55:36 +0800

    Update __init__.py
    make  lhb_detais available

--- tushare/__init__.py
+++ tushare/__init__.py
@@ -67,7 +67,7 @@ from tushare.stock.shibor import (shibor_data, shibor_quote_data,
 for LHB
 """
 from tushare.stock.billboard import (top_list, cap_tops, broker_tops,
-                                     inst_tops, inst_detail)
+                                     inst_tops, inst_detail, lhb_details)
 
 
 """
